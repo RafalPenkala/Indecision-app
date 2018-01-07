8638ad35016943dd8c3d7ad6a530a95c66df65fb
RafalPenkala <raf.tricity@gmail.com>
2018-01-07 22:52:17 +0100

    Classes part 2 - subclass "Traveler"

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,11 +13,48 @@ class Person {
   }
 }
 
-const me = new Person('Fufu the Killer', 32);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-const other = new Person ();
-console.log(other.getGreeting());
-console.log(other.getDescription());
+  hasMajor() {
+    return !!this.major;
+  }
+
+  get getDescription() {
+  let description = super.getDescription();
+
+  if(this.hasMajor()) {
+  description += ` His major is ${this.major}.`
+  }
+  return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  homeLocation() {
+    return !!this.homeLocation;
+  }
+
+  get getGreeting() {
+    let greeting = super.getGreeting();
+    if (this.homeLocation) {
+      greeting +=  ` I'm visiting from ${this.homeLocation}.`
+    }
+    return greeting;
+  }
+}
+
+const me = new Traveler('Fufu the Killer', 32, 'Gdansk');
+console.log(me.getGreeting);
+
+const other = new Traveler();
+console.log(other.getGreeting);
 
